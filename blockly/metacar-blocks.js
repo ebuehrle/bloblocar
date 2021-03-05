5b44c030bad073bd6a23e1fa14dcc100b481226c
Etienne Bührle <43623224+ebuehrle@users.noreply.github.com>
2021-03-05 19:38:07 +0100

    Initial commit

---
+++ blockly/metacar-blocks.js
@@ -0,0 +1,76 @@
+Blockly.Blocks['forward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("vorwärts");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Vorwärts fahren.");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['turn'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["nach links abbiegen","left"], ["nach rechts abbiegen","right"]]), "turn_direction");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Auf der Stelle drehen.");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['backward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("rückwärts");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Rückwärts fahren.");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['stop'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("stop");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Stehenbleiben.");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['sleep'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("warte")
+        .appendField(new Blockly.FieldNumber(500, 0, Infinity, 1), "millis")
+        .appendField("Millisekunden");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(230);
+ this.setTooltip("Eine gewisse Zeit abwarten und weiterfahren.");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['lidar'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("da ist")
+        .appendField(new Blockly.FieldDropdown([["ein Auto","car"], ["eine Straße","road"], ["etwas anderes","terrain"]]), "obstacle_type")
+        .appendField(new Blockly.FieldDropdown([["2m hinter mir","0m"], ["1m hinter mir","1m"], ["0m vor mir","2m"], ["1m vor mir","3m"], ["2m vor mir","4m"]]), "front_distance")
+        .appendField(new Blockly.FieldDropdown([["0m","0m"], ["1m","1m"], ["2m","2m"]]), "lateral_distance")
+        .appendField(new Blockly.FieldDropdown([["links von mir","left"], ["rechts von mir","right"]]), "lateral_direction");
+    this.setOutput(true, "Boolean");
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ js/index.js
@@ -0,0 +1,68 @@
+const env = new metacar.env('metacar', metacar.level.level3);
+env.setAgentLidar({pts: 5, width: 4, height: 5, pos: -1.5});
+env.load();
+
+window.addEventListener('keydown', e => {
+    if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) {
+        e.preventDefault();
+    }
+});
+
+let workspace = Blockly.inject('blockly', {
+    toolbox: document.getElementById('toolbox')
+});
+
+workspace.addChangeListener(() => {
+    const xml = Blockly.Xml.workspaceToDom(workspace);
+    const text = Blockly.Xml.domToText(xml);
+    localStorage.setItem('workspaceContents', text);
+});
+
+(function loadWorkspace() {
+    if ('workspaceContents' in localStorage) {
+        const text = localStorage.getItem('workspaceContents');
+        const xml = Blockly.Xml.textToDom(text);
+        Blockly.Xml.domToWorkspace(xml, workspace);
+    }
+})();
+
+let codeInterpreter;
+
+document.querySelector('.run').addEventListener('click', () => {
+    Blockly.JavaScript.addReservedWords('code', 'env');
+    Blockly.JavaScript.INFINITE_LOOP_TRAP = 'sleep(1);\n';
+    const code = Blockly.JavaScript.workspaceToCode(workspace);
+
+    console.log(code);
+
+    new Promise(resolve => {
+        codeInterpreter = new Interpreter(code, initMetacarApi(env));
+        (function runCode() {
+            document.querySelector('.execution').classList.add('running');
+            try {
+                while (codeInterpreter && codeInterpreter.step()) {
+                    const stepTimeout = codeInterpreter['stepTimeout'] || 0;
+                    codeInterpreter['stepTimeout'] = 0;
+                    if (stepTimeout) {
+                        setTimeout(runCode, stepTimeout);
+                        return;
+                    }
+                }
+            } catch (e) {
+                alert(e);
+            }
+            document.querySelector('.execution').classList.remove('running');
+        })();
+        resolve();
+    });
+});
+
+document.querySelector('.stop').addEventListener('click', () => {
+    codeInterpreter.paused_ = true;
+    codeInterpreter = null;
+    env.level.agent.core.v = 0
+});
+
+document.querySelector('.reset').addEventListener('click', () => {
+    env.reset();
+});

915f12d032f22459f0498bb5356d82aa3b84c308
Etienne Bührle <43623224+ebuehrle@users.noreply.github.com>
2021-04-10 19:31:10 +0200

    Add Blockly solutions

--- js/spoilers.js
+++ js/spoilers.js
@@ -1,3 +1,33 @@
 document.querySelectorAll('.spoiler').forEach(e => {
     e.addEventListener('click', () => e.classList.toggle('open'))
 });
+
+/* Blockly doesn't like being initialized in hidden divs. */
+(function openAllSpoilers() {
+    document.querySelectorAll('.spoiler').forEach(e => e.classList.add('open'));
+})();
+
+document.querySelectorAll('.spoiler-blockly').forEach(e => {
+    e.addEventListener('click', e => e.stopImmediatePropagation());
+
+    const workspace = Blockly.inject(e.id, {
+        toolbox: toolboxDefinition,
+        readOnly: true,
+        scrollbars: true,
+    });
+
+    if (!e.dataset.src) {
+        return;
+    }
+
+    fetch(e.dataset.src)
+        .then(result => result.text())
+        .then(xmlText => {
+            const xml = Blockly.Xml.textToDom(xmlText);
+            Blockly.Xml.domToWorkspace(xml, workspace);
+        });
+});
+
+(function closeAllSpoilers() {
+    document.querySelectorAll('.spoiler').forEach(e => e.classList.remove('open'))
+})();
